161fea28198ed3cd3c6f1c90137bf3e6a51224e9
pasz <saveseam@gmail.com>
2024-04-12 22:22:59 +0200

    Clear some code TimerFragment

--- app/src/main/java/com/sarnavsky/pasz/nighlight2/fragments/TimerFragment.kt
+++ app/src/main/java/com/sarnavsky/pasz/nighlight2/fragments/TimerFragment.kt
@@ -31,53 +31,9 @@ class TimerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val itemsHours = arrayOf(0, 1, 2, 3, 4, 6)
-        val adapterHours = ArrayAdapter(requireContext(), R.layout.simple_spinner_item, itemsHours)
-        adapterHours.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
-        binding.spinnerHours.adapter = adapterHours
-        binding.spinnerHours.setSelection(0)
-        val itemsMinutes = arrayOf(0, 1, 5, 10, 20, 30, 40, 50)
-        val adapterMinutes =
-            ArrayAdapter(requireContext(), R.layout.simple_spinner_item, itemsMinutes)
-        adapterMinutes.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
-        binding.spinnerMinutes.adapter = adapterMinutes
-        binding.spinnerMinutes.setSelection(0)
-        binding.noButton.setOnClickListener { removeThisFragment() }
-
-        binding.setButton.setOnClickListener {
-            val timerMilliseconds = convertHoursAndMinutesToMilliseconds(
-                binding.spinnerHours.selectedItem as Int,
-                binding.spinnerMinutes.selectedItem as Int
-            )
-
-            if (timerMilliseconds > 0) {
-                myTimer.timerDuration = timerMilliseconds
-                myTimer.timerStatus = true
-                myTimer.lastTimerHour = binding.spinnerHours.selectedItemPosition
-                myTimer.lastTimerMinute = binding.spinnerMinutes.selectedItemPosition
-                viewModel.updateTimer(myTimer)
-            }else{
-                myTimer.timerDuration = 0
-                myTimer.timerStatus = false
-                myTimer.lastTimerHour = binding.spinnerHours.selectedItemPosition
-                myTimer.lastTimerMinute = binding.spinnerMinutes.selectedItemPosition
-
-                viewModel.updateTimer(myTimer)
-            }
-
-
-            //val mainFragment =
-              //  parentFragmentManager.findFragmentByTag("main_fragment") as MainFragment?
-           // if (mainFragment != null) {
-//                mainFragment.startTimer(
-//                    binding.spinnerHours.selectedItem as Int,
-//                    binding.spinnerMinutes.selectedItem as Int
-//                )
-                removeThisFragment()
-           // }
-        }
-
         observer()
+        initListener()
+        initAdapter()
     }
 
     private fun removeThisFragment() {
@@ -86,9 +42,7 @@ class TimerFragment : Fragment() {
     }
 
     private fun observer() {
-
         viewModel.getTimer()
-
         viewModel.timerLiveData.observe(viewLifecycleOwner) {
             if (it == null) {
                 viewModel.insertTimer()
@@ -101,9 +55,48 @@ class TimerFragment : Fragment() {
         }
     }
 
-
-     private fun convertHoursAndMinutesToMilliseconds(hours: Int, minutes: Int): Int {
+    private fun convertHoursAndMinutesToMilliseconds(hours: Int, minutes: Int): Int {
         val totalMinutes = hours * 60 + minutes
         return totalMinutes * 60 * 1000
     }
+
+    private fun initListener() {
+        binding.noButton.setOnClickListener { removeThisFragment() }
+        binding.setButton.setOnClickListener {
+            val timerMilliseconds = convertHoursAndMinutesToMilliseconds(
+                binding.spinnerHours.selectedItem as Int,
+                binding.spinnerMinutes.selectedItem as Int
+            )
+
+            if (timerMilliseconds > 0) {
+                myTimer.timerDuration = timerMilliseconds
+                myTimer.timerStatus = true
+            } else {
+                myTimer.timerDuration = 0
+                myTimer.timerStatus = false
+            }
+
+            myTimer.lastTimerHour = binding.spinnerHours.selectedItemPosition
+            myTimer.lastTimerMinute = binding.spinnerMinutes.selectedItemPosition
+            viewModel.updateTimer(myTimer)
+            removeThisFragment()
+        }
+    }
+
+    private fun initAdapter() {
+
+        val itemsHours = arrayOf(0, 1, 2, 3, 4, 6)
+        val adapterHours = ArrayAdapter(requireContext(), R.layout.simple_spinner_item, itemsHours)
+        adapterHours.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        binding.spinnerHours.adapter = adapterHours
+        binding.spinnerHours.setSelection(0)
+
+        val itemsMinutes = arrayOf(0, 1, 5, 10, 20, 30, 40, 50)
+        val adapterMinutes =
+            ArrayAdapter(requireContext(), R.layout.simple_spinner_item, itemsMinutes)
+        adapterMinutes.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        binding.spinnerMinutes.adapter = adapterMinutes
+        binding.spinnerMinutes.setSelection(0)
+    }
+
 }
