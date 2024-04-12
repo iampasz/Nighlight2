117c10c72358218a11fd3e90f974efc2c8213835
pasz <saveseam@gmail.com>
2024-04-12 22:14:24 +0200

    Clear some code in MainFragment

--- app/src/main/java/com/sarnavsky/pasz/nighlight2/util/Const.kt
+++ app/src/main/java/com/sarnavsky/pasz/nighlight2/util/Const.kt
@@ -9,17 +9,3 @@ const val MY_SETTINGS = "my_settings"
  const val ANIMATION_BUTTON = 5
  const val TIMER_BUTTON = 6
  const val BRIGHTS_BUTTON = 7
-//
-// const val EMPTY_SOUND = 7
-// const val FIRST_SOUND = 8
-// const val SECOND_SOUND = 9
-//
-// const val RED_COLOR = 10
-// const val WHITE_COLOR = 11
-// const val BLACK_COLOR = 12
-// const val ORANGE_COLOR = 13
-// const val YELLOW_COLOR = 14
-// const val GREEN_COLOR = 15
-// const val CANYAN_COLOR = 16
-// const val BLUE_COLOR = 17
-// const val MOOD_COLOR = 18
